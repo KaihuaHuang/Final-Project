058fa015d0e92057246ce4d9301558454b0521ca
Xiaoshan Lin <linxiaoshan1994@gmail.com>
2018-03-24 15:13:30 -0400

    1

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 	cout << "Hello World";
 	cout << " Angela is able to edit.";
-        cout << "Rui Ma";
+    cout << "Rui Ma";
 	cout<<"Zenghui Li"<<endl;
 	cout << "111" << endl;
 	cout << "ANGELA" << endl;
