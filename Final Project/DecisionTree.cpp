e5a7f492ddf9ed6e8be15b04664d453e411e6b19
Xiaoshan Lin <linxiaoshan1994@gmail.com>
2018-04-12 02:51:36 -0400

    temp

---
+++ Final Project/DecisionTree.cpp
@@ -0,0 +1,10 @@
+#include "DecisionTree.h"
+using namespace std;
+
+double DecisionTree::computeEntropy(vector<Node>& dataSet) {}
+
+double DecisionTree::computeGain(vector<Node>& dataSet) {
+
+}
+
+double DecisionTree::isLeafNode() {}
--- Final Project/DecisionTree.h
+++ Final Project/DecisionTree.h
@@ -1,10 +1,29 @@
-#pragma once
+#ifndef DECISIONTREE_H
+#define DECISIONTREE_H
+
+#include <iostream>
 #include <vector>
+#include <map>
+#include <utility>
+#include <tuple>
+
 #include "Node.h"
+using namespace std;
 
 class DecisionTree {
 private:
+	// map< ID, tuple<tags of 0&1, label> >
+	map<string, tuple<vector<int>,int>> IDcode;
+	// vector< index of factor, cut-off point >
+	vector<pair<int,int>> Attributes;
+	double StopCriteria;
 
 public:
-	double* computeEntropy();
+	DecisionTree(int p = 0.95) : StopCriteria(p) {}
+	double computeEntropy(vector<Node>& dataSet);
+	double computeGain(vector<Node>& dataSet);
+	double isLeafNode();
 };
+
+#endif // DECISIONTREE_H
+
