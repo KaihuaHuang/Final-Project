6d6dda8d49112d8c9b269f37de3da46e11eb648e
William Huang <khuang41@fordham.edu>
2018-04-13 00:18:36 -0400

    KNN predict node implementation

--- Final Project/KNN.cpp
+++ Final Project/KNN.cpp
@@ -1,5 +1,7 @@
 #include "KNN.h"
 #include <vector>
+#include <map>
+
 
 KNN::KNN(int k, double * inputWeights, int weightNum)
 {
@@ -45,11 +47,37 @@ int * KNN::predict(std::vector<Node>& dataSet, const int & num)
 
 int KNN::predictNode(Node & node)
 {
-	std::vector<double> distances;
+	std::multimap<double, int> index_distance;
+	int index = 0;
 	for (auto& e : trainningSet) {
-		distances.push_back(distance(node, e, weights));
+		index_distance.insert(std::pair<double, int>(distance(node, e, weights), index));
+		index++;
 	}
-	return 0;
+
+	std::map<int, int> labelCounter;
+	int count = 0;
+	for (auto it = index_distance.begin(); it != index_distance.end() & count < kNeighbor; it++,count++) {
+		int index = it->second;
+		int label = trainningSet[index].getLabel();
+		try {
+			labelCounter.at(label);
+			labelCounter[label] = labelCounter[label] + 1;
+		}
+		catch (std::exception & e) {
+			labelCounter[label] = 1;
+		}
+	}
+
+	int finalLabel = -1;
+	int highestCount = -1;
+	for (auto& e : labelCounter) {
+		//If they are equal what shold you do
+		if (e.second > highestCount) {
+			highestCount = e.second;
+			finalLabel = e.first;
+		}
+	}
+	return finalLabel;
 
 }
 
--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -2,16 +2,22 @@
 #include <string>
 #include "Node.h"
 #include "ExcelReader.h"
+#include "KNN.h"
 using namespace std;
 
 int main() {
 	string fileName("dataSet.csv");
 	ExcelReader reader(fileName);
 	vector<Node> dataSet = reader.readFile();
-	for (auto it = dataSet.begin(); it != dataSet.end(); it++) {
+	/*for (auto it = dataSet.begin(); it != dataSet.end(); it++) {
 		cout << *it << endl;
-	}
+	}*/
 	double* weights = NULL;
-	cout << distance(dataSet[0], dataSet[1], weights) << endl;
+	//cout << distance(dataSet[0], dataSet[1], weights) << endl;
+
+	KNN cif;
+	cif.fit(dataSet, dataSet.size());
+	cout << "Predict Label: " << cif.predictNode(dataSet[0]) << endl;
+
 
 }
